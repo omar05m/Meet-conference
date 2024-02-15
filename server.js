d19b74ee48cb104281324a48fe740cfdf0523bc9
omar05m <omarkof99@gmail.com>
2024-02-15 19:27:09 +0100

    adding public folder

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const {v4: uuidv4  } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 
 //specifie moteur
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get('/',(req , res) => {
-res.redirect('/${uuidv4()}');
+res.redirect(`/${uuidv4()}`);
 });
 
 //creation roomID
 app.get('/:room',(req, res) =>{
     res.render('room',{ roomId: req.params.room});
 });
-
-
 
 //le port de serveur 
 server.listen(3030);
