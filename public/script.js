f95a3d4faf0fa0fd8ace36353c193ab9b5f1ea32
omar05m <omarkof99@gmail.com>
2024-02-16 21:51:47 +0100

    chat messaging features

--- public/script.js
+++ public/script.js
@@ -53,3 +53,18 @@ const addVideoStream = (video, stream) =>{
     })
     videoGrid.append(video);
 }
+
+let text = $("input");
+
+// when press enter button send message
+$('html').keydown(function (e) {
+  if (e.which == 13 && text.val().length !== 0) {
+    socket.emit('message', text.val());
+    text.val('')
+  }
+});
+
+socket.on("createMessage", message => {
+    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+    scrollToBottom()
+})
