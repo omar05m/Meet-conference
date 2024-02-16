5b3487c7e408c739ffb50486b306de556bc63fb7
omar05m <omarkof99@gmail.com>
2024-02-16 17:30:16 +0100

    adding ability to display two screen

--- public/script.js
+++ public/script.js
@@ -18,6 +18,14 @@ navigator.mediaDevices.getUserMedia({
 }).then(stream =>{
     myVideoStream = stream;
     addVideoStream(myVideo,stream)
+    
+    peer.on('call',call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video,userVideoStream)
+        })
+    })
 
     socket.on('user-connected',(userId) =>{
         connecToNewUser(userId, stream);
